7bcf88c18b1a315873acacdb89ee06413ed1a494
heke1228 <chengkelfan@qq.com>
2026-01-12 12:45:46 +0800

    update: the c1 template

--- templates/c1/src/main.rs
+++ templates/c1/src/main.rs
@@ -5,6 +5,11 @@ use ecos_ssc1::{ecos_main, println};
 
 #[ecos_main]
 fn main() -> ! {
+    loop {
+        if '\n' as u8 == ecos_ssc1::Uart::read_byte_blocking() {
+            break;
+        }
+    }
     println!("Hello ECOS from Rust Project [{{project_name}}]!!!");
     loop {}
 }
